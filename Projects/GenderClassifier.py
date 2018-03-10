af5a0f0053d5f68a3155aeaed7367fea0b8a0fc0
PauloLopez <8420448+4ToolMaker@users.noreply.github.com>
2018-03-09 23:23:52 -0800

    Update GenderClassifier.py

--- Projects/GenderClassifier.py
+++ Projects/GenderClassifier.py
@@ -1,1 +1,9 @@
-print('Hello')
+# importing the decision tree library within scikit-learn
+
+from sklearn import tree 
+
+# creating our data randomly giving 3 different features as a list of list
+# [height, weight, shoe size] 
+
+X = [[177, 200, 37], [160, 58, 38], [177, 70, 43], [], ]
+
